7cf115a03ae9f2b421101bfe9fde223b81c393ff
Marcel Dechant <marcel.dechant@freenet.de>
2025-01-22 13:41:38 +0100

    added forgot-password-page & reset-pasword-page

---
+++ src/app/forgot-password-page/forgot-password-page.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormInputComponent } from '../shared/components/form-input/form-input.component';
+import { BackendApiService } from '../services/backend_api/backend-api.service';
+import { FormService } from '../services/form/form.service';
+import { MessageToastService } from '../services/message_toast/message-toast.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-forgot-password-page',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ReactiveFormsModule,
+    FormInputComponent,
+  ],
+  templateUrl: './forgot-password-page.component.html',
+  styleUrl: './forgot-password-page.component.scss'
+})
+export class ForgotPasswordPageComponent {
+
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private bs: BackendApiService,
+    private formService: FormService,
+    private messageToastService: MessageToastService,
+    private router: Router,
+  ) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+}
+
+  /**
+   * Handles form submission, triggers validation if the form is invalid,
+   * otherwise initiates the forgot password process.
+   */
+  onSubmit() {
+    if (this.form.valid) {
+      const email = this.form.get('email')?.value;
+      this.handleForgotPassword(email);
+    } else {
+      this.formService.validateAllFormFields(this.form);
+    }
+  }
+
+  /**
+   * Performs the forgot password request and handles the response or error.
+   * @param email - The user's email address.
+   */
+  async handleForgotPassword(email: string) {
+    try {
+      const response: any = await this.bs.forgotPassword(email);
+      console.log(response);
+      this.messageToastService.setSuccess(response.detail);
+      this.router.navigate(['/login'])
+    } catch (error: any) {
+      console.error(error);
+      this.messageToastService.setError(error.error.detail);
+    }
+  }
+}
---
+++ src/app/reset-password-page/reset-password-page.component.ts
@@ -0,0 +1,125 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormInputComponent } from '../shared/components/form-input/form-input.component';
+import { passwordsMatchValidator } from '../sign-up-page/validators/passwords_match.validator';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BackendApiService } from '../services/backend_api/backend-api.service';
+import { FormService } from '../services/form/form.service';
+import { MessageToastService } from '../services/message_toast/message-toast.service';
+@Component({
+  selector: 'app-reset-password-page',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ReactiveFormsModule,
+    FormInputComponent,
+  ],
+  templateUrl: './reset-password-page.component.html',
+  styleUrl: './reset-password-page.component.scss'
+})
+export class ResetPasswordPageComponent {
+  /**
+   * Form group for handling form inputs.
+   */
+  form: FormGroup;
+
+  uid: string | null = '';
+  token: string | null = '';
+  isValidUrl: boolean = false;
+
+  /**
+   * Constructor to inject services and initialize the form group.
+   * @param fb - FormBuilder instance for creating the reactive form.
+   * @param bs - BackendApiService for handling API requests.
+   * @param route - ActivatedRoute for query parameters.
+   * @param formService - FormService for form validation utilities.
+   * @param messageToastService - MessageToastService for displaying messages.
+   */
+  constructor(
+    private fb: FormBuilder,
+    private bs: BackendApiService,
+    private route: ActivatedRoute,
+    private formService: FormService,
+    private messageToastService: MessageToastService,
+    private router: Router,
+  ) {
+    this.form = this.fb.group(
+      {
+        password: ['', [Validators.required]],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        validator: passwordsMatchValidator('password', 'confirmPassword'),
+      } as AbstractControlOptions
+    );
+  }
+
+  ngOnInit() {
+    // get uid and token
+    this.uid = this.route.snapshot.queryParamMap.get('uid');
+    this.token = this.route.snapshot.queryParamMap.get('token');
+
+    // check if url is valid
+    if (this.uid && this.token) {
+      this.isValidUrl = true;
+    }
+  }
+
+
+  ngAfterViewInit() {
+    // show invalid link error if url is invalid
+    if (!this.isValidUrl) {
+      setTimeout(() => {
+        this.messageToastService.setError('Invalid link.');
+      });
+    }
+  }
+
+
+  /**
+   * Handles form submission, triggers validation if the form is invalid,
+   * otherwise initiates the reset password process.
+   */
+  onSubmit() {
+    if (this.form.valid && this.isValidUrl) {
+      const password = this.form.get('password')?.value;
+      this.handleResetPassword(password);
+    } else {
+      this.formService.validateAllFormFields(this.form);
+    }
+  }
+
+
+  /**
+   * Performs the reset password request and handles the response or error.
+   * @param password - The user's new password.
+   */
+  async handleResetPassword(password: string) {
+    try {
+      const response: any = await this.bs.resetPassword(
+        password,
+        this.uid!,
+        this.token!
+      );
+      console.log(response);
+      this.messageToastService.setSuccess(
+        'The password has been successfully reset.'
+      );
+      this.router.navigate(['/login'])
+    } catch (error: any) {
+      console.error(error);
+      this.messageToastService.setError(
+        'Invalid link. Please request a new link.'
+      );
+    }
+  }
+}
